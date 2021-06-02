eaf8a663bbf7c923bfbcea97aa80f3c6e35fd780
3251172193 <80187156+3251172193@users.noreply.github.com>
2021-06-02 15:19:38 +0800

    Add files via upload

---
+++ week4/src/work1/AddressDrive.java
@@ -0,0 +1,8 @@
+package work1;
+
+public class AddressDrive {
+	public static void main(String[] args) {
+	    Address A = new Address("China", "Hubei", "Wuhan", "jiedao", 000);
+	    A.PrintAddress();
+	}
+}
---
+++ week4/src/work2/DogDrive.java
@@ -0,0 +1,8 @@
+package work2;
+
+public class DogDrive {
+	public static void main(String[] args) {
+        Dog x = new Dog("x", "black", 3);
+        x.displayDog();
+    }
+}
